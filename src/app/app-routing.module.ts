db3fad377660e0c19b86b2eddbdc5e6efb542d6f
vinicius magalhaes de souza <vmsouza@localhost.localdomain>
2020-12-21 18:52:55 -0300

    antes de refatorar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule'},
+  { path: 'entries', loadChildren: './pages/entries/entries.module#EntriesModule'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
